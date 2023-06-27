8a6f3db0ea88aa4d3924adf268a595ad6e2fd8dd
Asabeneh <asabeneh@gmail.com>
2023-06-28 02:47:16 +0300

    arithmetic added

--- hello.py
+++ hello.py
@@ -1,1 +1,14 @@
 print('Hello World!')
+""" 
+Let us add 
+more Python script here
+
+"""
+
+# Arithmetics using Python
+a = 4
+b = 3
+print(f'{a} + {b} = {a + b}')
+print(f'{a} - {b} = {a - b}')
+print(f'{a} * {b} = {a * b}')
+print(f'{a} / {b} = {a / b}')
